fe142972c813d3310d70193c0ca9ea6bf0885ba1
krik9 <krik9@krik9.com>
2020-04-18 05:34:42 +1000

    working example

---
+++ components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function App(props){
+    return (
+        <div>
+            <h1> Hello from inside App component {props.name}</h1>
+        </div >
+    )
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "../components/App"
+
+if (module.hot) {
+    module.hot.accept()
+}
+
+
+var rootElement = document.getElementById('root')
+ReactDOM.render(<App name={'jim'} />, rootElement)
